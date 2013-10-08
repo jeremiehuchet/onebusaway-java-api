673ef09608946d1d0264007ec31221eed0a74241
kops <jeremie@dudie.fr>
2013-10-09 01:24:49 +0200

    refactor unit test to be more unit than integration

--- src/test/java/fr/dudie/onebusaway/client/JsonOneBusAwayClientTest.java
+++ src/test/java/fr/dudie/onebusaway/client/JsonOneBusAwayClientTest.java
@@ -14,15 +14,15 @@
  */
 package fr.dudie.onebusaway.client;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
-import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
-import java.util.Properties;
-
-import junit.framework.TestCase;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.client.HttpClient;
@@ -32,9 +32,13 @@ import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.SingleClientConnManager;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fr.dudie.junit.rules.RunWithWebServer;
 import fr.dudie.onebusaway.model.ArrivalAndDeparture;
 import fr.dudie.onebusaway.model.ScheduleStopTime;
 import fr.dudie.onebusaway.model.Stop;
@@ -47,35 +51,23 @@ import fr.dudie.onebusaway.model.TripSchedule;
  * @author Jérémie Huchet
  * @author Olivier Boudet
  */
-public class JsonOneBusAwayClientTest extends TestCase {
+public class JsonOneBusAwayClientTest {
 
     /** The event logger. */
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonOneBusAwayClientTest.class);
 
+    /** Run a web server. */
+    @Rule
+    public static final RunWithWebServer SERVER = new RunWithWebServer("/www");
+
     /** The tested OneBusAway client. */
-    private static JsonOneBusAwayClient obaClient;
-
-    /** Path to the properties file. */
-    private static final String PROPS_PATH = "/onebusaway-client-test.properties";
-
-    /** Loaded properties. */
-    private static final Properties PROPS = new Properties();
+    private JsonOneBusAwayClient obaClient;
 
     /**
-     * Instantiates the test.
-     * 
-     * @param name
-     *            the test name
-     * @throws IOException
-     *             an error occurred during initialization
+     * Setup the OneBusAway client.
      */
-    public JsonOneBusAwayClientTest(final String name) throws IOException {
-
-        super(name);
-
-        LOGGER.info("Loading configuration file {}", PROPS_PATH);
-        final InputStream in = JsonOneBusAwayClientTest.class.getResourceAsStream(PROPS_PATH);
-        PROPS.load(in);
+    @Before
+    public void setupOneBusAwayClient() {
 
         LOGGER.info("Preparing http client");
         final SchemeRegistry registry = new SchemeRegistry();
@@ -83,11 +75,7 @@ public class JsonOneBusAwayClientTest extends TestCase {
         final ClientConnectionManager connexionManager = new SingleClientConnManager(null, registry);
 
         final HttpClient httpClient = new DefaultHttpClient(connexionManager, null);
-
-        final String url = PROPS.getProperty("onebusaway.api.url");
-        final String key = PROPS.getProperty("onebusaway.api.key");
-        obaClient = new JsonOneBusAwayClient(httpClient, url, key);
-
+        obaClient = new JsonOneBusAwayClient(httpClient, SERVER.getUrl().toString(), "test");
     }
 
     /**
@@ -96,6 +84,7 @@ public class JsonOneBusAwayClientTest extends TestCase {
      * @throws IOException
      *             an error occurred
      */
+    @Test
     public final void testGetTripDetails() throws IOException {
 
         LOGGER.info("testGetTripDetails.start");
@@ -104,9 +93,9 @@ public class JsonOneBusAwayClientTest extends TestCase {
         schedule = obaClient.getTripDetails("2_10000");
 
         assertNotNull("no schedule returned by the api", schedule);
-        assertEquals("27 stop times should be returned by the api", 27, schedule.getStopTimes()
+        assertEquals("20 stop times should be returned by the api", 20, schedule.getStopTimes()
                 .size());
-        assertEquals("the first stop of this trip should be Saint Saëns", "Saint Saëns", schedule
+        assertEquals("the first stop of this trip should be Henri Fréville", "Henri Fréville", schedule
                 .getStopTimes().get(0).getStop().getName());
 
         LOGGER.info("testGetTripDetails.end");
@@ -118,6 +107,7 @@ public class JsonOneBusAwayClientTest extends TestCase {
      * @throws IOException
      *             an error occurred
      */
+    @Test
     public final void testGetArrivalsAndDeparturesForStop() throws IOException {
 
         LOGGER.info("testGetArrivalsAndDeparturesForStop.start");
@@ -147,6 +137,7 @@ public class JsonOneBusAwayClientTest extends TestCase {
      * @throws IOException
      *             an error occurred
      */
+    @Test
     public final void testGetStop() throws IOException {
 
         LOGGER.info("testGetStop.start");
@@ -172,6 +163,7 @@ public class JsonOneBusAwayClientTest extends TestCase {
      * @throws ParseException
      *             The date is not in a valid format
      */
+    @Test
     public final void testGetScheduleForStop() throws IOException, ParseException {
 
         LOGGER.info("testGetScheduleForStop.start");
@@ -184,7 +176,7 @@ public class JsonOneBusAwayClientTest extends TestCase {
 
         assertNotNull("no schedule returned by the api", schedule);
 
-        assertEquals("79 stop times should be returned by the api", 79, schedule.getStopTimes()
+        assertEquals("172 stop times should be returned by the api", 172, schedule.getStopTimes()
                 .size());
         assertEquals("the stop id should be 2_1167", "2_1167", schedule.getStop().getId());
         assertEquals("the stop name should be République Pré Botté", "République Pré Botté",
@@ -198,7 +190,7 @@ public class JsonOneBusAwayClientTest extends TestCase {
                 cpt++;
             }
         }
-        assertEquals("31 stop times should be returned by the api for line 53", 31, cpt);
+        assertEquals("87 stop times should be returned by the api for line 53", 87, cpt);
 
         LOGGER.info("testGetScheduleForStop.end");
     }
